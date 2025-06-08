bd954e231a73e52e99c78fc893d727afca4a55d8
kartikeysahu987 <kartikeysahu987@gmail.com>
2025-06-08 14:44:33 +0530

    JWT commit

--- main.go
+++ main.go
@@ -23,12 +23,6 @@ func main(){
 	routes.UserRoutes(router)
 
 
-	// router.GET("/api-1",func(c*gin.Context){
-	// 	c.JSON(200,gin.H{"success":"Access granted for api-1"})
-	// })
-	// router.GET("/api-2",func(c*gin.Context){
-	// 	c.JSON(200,gin.H{"success":"Access granted for api-2"})
-	// })
 
 	router.Run(":"+port)	
 
